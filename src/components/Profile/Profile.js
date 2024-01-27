66a56a879154a3de3d71b80e6a6296729ac20fd8
OlegKrivenko <krivenko.olegus@gmail.com>
2024-01-27 15:02:13 +0200

    modify component Profile, props

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,9 @@
 import PropTypes from 'prop-types';
 import css from './Profile.module.css';
 
-function Profile({ username, tag, location, avatar, stats }) {
+function Profile({ user }) {
+  const { username, tag, location, avatar, stats } = user;
+
   return (
     <div className={css.profile}>
       <div className={css.description}>
@@ -30,14 +32,16 @@ function Profile({ username, tag, location, avatar, stats }) {
 }
 
 Profile.propTypes = {
-  username: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  stats: PropTypes.shape({
-    followers: PropTypes.number.isRequired,
-    views: PropTypes.number.isRequired,
-    likes: PropTypes.number.isRequired,
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }),
   }),
 };
 
